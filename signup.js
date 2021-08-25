2cea9d83acb3d488e618801e84c7ab29ccf43094
shaista78 <shaistasulthana78@gmail.com>
2021-08-25 18:06:47 +0530

    condition update

--- signup.js
+++ signup.js
@@ -9,7 +9,7 @@
 // function set (){
 //     var holder = document.getElementById("nu");
 //     holder.style.background = '#E3F2FD';
-   
+
 // }
 
 //  function hol (){
@@ -17,61 +17,64 @@
 //      botn.style.background = 'red';
 // alert("yes")
 //  }
- var user = [];
- 
+var user = [];
 
- if(localStorage.getItem("user")==null){
 
-    localStorage.setItem("user",JSON.stringify([]));
+if (localStorage.getItem("user") == null) {
+
+    localStorage.setItem("user", JSON.stringify([]));
 }
 var againget = localStorage.getItem("user");
-if(againget !== null){
-    user= JSON.parse(againget);
+if (againget !== null) {
+    user = JSON.parse(againget);
 }
- function store (){
+function store() {
 
-     var inputmob = document.getElementById("nu").value;
-     var inputpass = document.getElementById("pas").value;
-     if(inputmob.length == 0 || inputmob.length < 10 || inputmob.length > 10){
-      
+    var inputmob = document.getElementById("nu").value;
+    var inputpass = document.getElementById("pas").value;
+    if (inputmob.length == 0 || inputmob.length < 10 || inputmob.length > 10) {
+
         var msg = document.getElementById("mess");
         msg.innerText = "Please enter vailid Number";
-         return;
-     }
-    else if(inputpass.length < 8){
+        return;
+    }
+    else if (inputpass.length < 8) {
         var msg = document.getElementById("mess1");
         msg.innerText = "Please enter min. 8 char";
 
-        return; 
+        return;
     }
 
-     if(localStorage.getItem("user") > 0){
-    var namedata = localStorage.getItem()
-     }
+    if (localStorage.getItem("user") > 0) {
+        var namedata = localStorage.getItem()
+    }
 
- 
+
     var objuser = {
 
         name: inputmob,
         password: inputpass,
     }
     user.push(objuser);
-  
+    var out = objuser.name;
     localStorage.setItem("user", JSON.stringify(user));
-check();
+    check(out);
 
- }
- 
- function check(){
+}
+
+function check(out) {
     var againget = localStorage.getItem("user");
-    if(againget !== null){
-        user= JSON.parse(againget);
+    console.log(user);
+    if (againget !== null) {
+        user = JSON.parse(againget);
     }
-    var inputmob = document.getElementById("nu").value;
-    
-    for(var i = 0; i < user.length; i++){
-        if(user.name[i]===inputmob){
+    if (user.some(function (el) {
+        if ((el.name === out)) {
             alert("congrats")
         }
-    }
- }
+    }));
+}
+
+
+
+
