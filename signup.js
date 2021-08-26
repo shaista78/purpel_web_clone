43eee7aabff392f246d4ce181987aa83e197fe13
rht16 <rohitkrrt@gmail.com>
2021-08-25 18:18:04 -0700

    checking

--- signup.js
+++ signup.js
@@ -1,77 +1,57 @@
-// var holder = document.getElementById("connect");
+var user = [];
 
 
-// function tes (){
-//     var holder = document.getElementById("nu");
-//     holder.style. = '#FFAB91';
-// //    holder.style. = '';
-// }
-// function set (){
-//     var holder = document.getElementById("nu");
-//     holder.style.background = '#E3F2FD';
-   
-// }
+if (localStorage.getItem("user") == null) {
 
-//  function hol (){
-//     var botn = document.getElementById("btn");
-//      botn.style.background = 'red';
-// alert("yes")
-//  }
- var user = [];
- 
-
- if(localStorage.getItem("user")==null){
-
-    localStorage.setItem("user",JSON.stringify([]));
+    localStorage.setItem("user", JSON.stringify([]));
 }
 var againget = localStorage.getItem("user");
-if(againget !== null){
-    user= JSON.parse(againget);
+if (againget !== null) {
+    user = JSON.parse(againget);
 }
- function store (){
+function store() {
 
-     var inputmob = document.getElementById("nu").value;
-     var inputpass = document.getElementById("pas").value;
-     if(inputmob.length == 0 || inputmob.length < 10 || inputmob.length > 10){
-      
+    var inputmob = document.getElementById("nu").value;
+    var inputpass = document.getElementById("pas").value;
+    if (inputmob.length == 0 || inputmob.length < 10 || inputmob.length > 10) {
+
         var msg = document.getElementById("mess");
         msg.innerText = "Please enter vailid Number";
-         return;
-     }
-    else if(inputpass.length < 8){
+        return;
+    }
+    else if (inputpass.length < 8) {
         var msg = document.getElementById("mess1");
         msg.innerText = "Please enter min. 8 char";
 
-        return; 
+        return;
     }
 
-     if(localStorage.getItem("user") > 0){
-    var namedata = localStorage.getItem()
-     }
+    if (localStorage.getItem("user") > 0) {
+        var namedata = localStorage.getItem()
+    }
 
- 
+
     var objuser = {
 
         name: inputmob,
         password: inputpass,
     }
     user.push(objuser);
-  
+    var out = objuser.name;
     localStorage.setItem("user", JSON.stringify(user));
-check();
+    check(out);
 
- }
- 
- function check(){
+}
+
+function check(out) {
     var againget = localStorage.getItem("user");
-    if(againget !== null){
-        user= JSON.parse(againget);
+    console.log(user);
+    if (againget !== null) {
+        user = JSON.parse(againget);
     }
-    var inputmob = document.getElementById("nu").value;
-    
-    for(var i = 0; i < user.length; i++){
-        if(user.name[i]===inputmob){
+    if (user.some(function (el) {
+        if ((el.name === out)) {
             alert("congrats")
         }
-    }
- }
+    }));
+}
